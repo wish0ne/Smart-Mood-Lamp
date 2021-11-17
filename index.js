643d8494fba72802d9e21d8e85be554f46d034b1
dpel959 <dpel959@naver.com>
2021-11-17 20:29:47 +0900

    5

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post("/api/text", (req,res) => {
         var textJSON = JSON.stringify(text);
         var parsedData = JSON.parse(textJSON);
         //fs.writeFileSync('./storage/texts/${userData.current_number}.json');
-				
+		console.log("success");
         //callback으로 flask에 정보요청
         res.send({"sentiments":[0.5,0.7,0.8,1.0,0.1,0.3,0.25,0.05,0.9]});
 });
