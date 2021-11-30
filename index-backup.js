65406906e34700dba6f5ec5e05cc4c5ed0149ca0
dpel959 <dpel959@naver.com>
2021-11-30 20:32:01 +0900

    deleted

--- index-backup.js
+++ index-backup.js
@@ -10,23 +10,6 @@ app.get("/", (req, res) => {
         res.send('Hello world\n')
 });
 
-app.post("/api/analysis", (req,res) => {
-        //analysis로, body로 emotion 보내면 된다. 단, json 형식으로.
-
-        //var userBuffer = fs.readFileSync('./storage/user_data.json');
-        //var userJSON = userBuffer.toString();
-        //var userData = JSON.parse(userJSON)
-
-        var emotion = req.body;
-
-        var emotionJSON = JSON.stringify(emotion);
-        var parsedData = JSON.parse(emotionJSON);//detailjson.negative 같이 거름
-
-        //fs.writeFileSync('./storage/analysis/${userData.current_number}.json', emotionJSON);
-
-        res.send('node server received emotion');
-});
-	  
 app.post("/api/text", (req,res) => {
         //"text" : 텍스트 형식으로.
         var text = req.body;
@@ -62,25 +45,6 @@ app.get("/api/text/exist", (req,res)=>{
 app.delete("/api/text/exist", (req,res)=>{
         var textid = req.query.textid;
 });
-
-app.post("/api/analysis/detail", (req,res) => {
-
-        //var userBuffer = fs.readFileSync('./storage/user_data.json');
-        //var userJSON = userBuffer.toString();
-        //var userData = JSON.parse(userJSON)
-
-        var detail = req.body; //json 파일으로 받는다.
-        //var emotion = req.body.emotion;
-        //var positive = req.body.positive;
-        //var neutral = req.body.neutral;
-        //var negative = req.body.negative;
-        var detailJSON = JSON.stringify(detail);
-        var parsedData = JSON.parse(detailJSON);//detailjson.negative 같이 거름
-
-        //fs.writeFileSync('./storage/analysis_detail/${userData.current_number}.json', detailJSON);
-        res.send("node server received analysis detail");
-});
-
 
 const {Server} = require('socket.io');
 
